f54789d18e2f8aad8c3ae49b191cceaaf87836ae
advmauryasandeep <adv.maurya.sandeep@gmail.com>
2026-05-25 17:06:27 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 export const metadata = {
   title: 'Lawmantras',
   description: 'Trusted legal educational platform',
@@ -7,6 +6,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HQZHZKHB17"></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HQZHZKHB17');
+            `,
+          }}
+        />
+      </head>
+
       <body
         style={{
           margin: 0,
